f7f30498b1bd554e6987261650eec80835df126c
sergiog <sergio.gutierrez@decameron.com>
2017-05-22 19:36:34 -0500

    Agregadas validaciones para funciones

--- index.php
+++ index.php
@@ -3,5 +3,9 @@
     $loginAPI = new LoginAPI();
     if ($_GET['action'] == "login") {
     	$loginAPI->ingresar();	
+    } else {
+    	$retorno = array();
+    	$retorno["error"] = "Funcion no encontrada";
+    	echo json_encode($retorno);
     }
     
