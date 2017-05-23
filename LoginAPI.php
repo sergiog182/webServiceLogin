95867b3ce97c5f518e9a0e4690c00dc0b5200d18
Sergio Gutierrez <sergioa.gutierrezs@gmail.com>
2017-05-22 19:18:30 -0500

    Cambios

--- LoginAPI.php
+++ LoginAPI.php
@@ -1,7 +1,10 @@
 <?php
-	class LoginAPI {    
-	    public function ingresar(){
+	class LoginAPI
+	{    
+	    public function ingresar()
+	    {
 	        header('Content-Type: application/JSON');                
+	        echo "<pre>" . var_export($_SERVER, true) . "</pre>";
 	        $method = $_SERVER['REQUEST_METHOD'];
 	        $respuesta = array();
 	        if ($_GET['user'] != "" && $_GET['password'] != "") {
